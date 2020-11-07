fa116d265fb75639f49b54aa77afa0a60fd01927
Lugixion <32132868+Lugixion@users.noreply.github.com>
2020-11-07 20:41:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,24 @@
 import tkinter as tk  
 from tkinter import filedialog
 
+def askforfile():
+    file_path = filedialog.askopenfilename(title="Select a file")
+
 root =tk.Tk()
 root.withdraw()
 
-file_path = filedialog.askopenfilename(title="Select a file")
+print("Select the file. (Have to be a .TXT)")
 
-file_path_parts = file_path.split(".")
+askforfile()
 
 # TXT Detection
 
-if "txt" in file_path_parts:
-    # File could be a .TXT, look if in his name have more than 1 dot
-    if len(file_path_parts) > 2:
-        if file_path_parts[len(file_path_parts) - 1] == "txt": # If last dot of the name have TXT
-            print("File is a TXT")
-        else:
-            print("File might be a .TXT")
-else:
-    # File isn't a .TXT
-    print("File might be a .TXT")
+if file_path.endswith(".txt"): # Ends with .txt
     
+else:
+    print("File might be a .TXT extension")
+    askforfile()
+
 # End of TXT Detection
 
 print(file_path)
